046afff29a1d0e7619adc9199734094cef6a8ee9
Slivinskaya Vera <slivinskaya.vera@gmail.com>
2024-03-15 17:42:56 +0300

    +

--- src/contexts/themeContext.jsx
+++ src/contexts/themeContext.jsx
@@ -3,12 +3,19 @@ import { createContext, useState } from "react";
 export const ThemeContext = createContext(null);
 
 export function ChangeThemeProvider({ children }) {
-  const [changeTheme, setChangeTheme] = useState(JSON.parse(localStorage.themeAll).key);
-  
+  if (localStorage.themeAll === undefined) {
+    localStorage.themeAll = JSON.stringify({ key: false });
+  }
+
+  const [changeTheme, setChangeTheme] = useState(
+    JSON.parse(localStorage.themeAll).key
+  );
+
   function handleThemeChange() {
     setChangeTheme((prev) => !prev);
   }
-  localStorage.themeAll = JSON.stringify({"key": changeTheme});
+
+  localStorage.themeAll = JSON.stringify({ key: changeTheme });
 
   return (
     <ThemeContext.Provider
